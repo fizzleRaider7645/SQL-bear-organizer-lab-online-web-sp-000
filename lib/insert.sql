a7bbeae1c3226e57c6baee2e8b29b4d3bc06407a
Douglas Uretsky <douglasuretsky@gmail.com>
2018-10-30 18:05:48 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'male', 'red', 'angry', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 3, 'male', 'black', 'lazy', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 3, 'female', 'grey', 'sleepy', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 3, 'male', 'yellow', 'spooked', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 3, 'female', 'silver', 'confused', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 3, 'male', 'purple', 'happy', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 3, 'female', 'blonde', 'sad', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 12, 'male', 'red', 'angry', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 34, 'male', 'black', 'lazy', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 16, 'female', 'grey', 'sleepy', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 5, 'male', 'yellow', 'spooked', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 6, 'female', 'silver', 'confused', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 7, 'male', 'purple', 'happy', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 2, 'female', 'blonde', 'sad', true);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 3, 'male', 'blue', 'nothingness', false);
