fcf933cb1cf36e8d761429d8c18fdebc4c524e77
Douglas Uretsky <douglasuretsky@gmail.com>
2018-10-30 18:03:55 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,7 +1,7 @@
 INSERT INTO bears (name, age, gender, color, temperament. alive) VALUES ('Mr. Chocolate', 3, 'male', 'red', 'angry', true);
-INSERT INTO bears (name, age, gender, color, temperament. alive) VALUES ('Rowdy', 3, 'male', 'red', 'angry', true);
-INSERT INTO bears (name, age, gender, color, temperament. alive) VALUES ('Tabitha', 3, 'female', 'red', 'angry', true);
-INSERT INTO bears (name, age, gender, color, temperament. alive) VALUES ('Sergeant Brown', 3, 'male', 'red', 'angry', true);
-INSERT INTO bears (name, age, gender, color, temperament. alive) VALUES ('Melissa', 3, 'female', 'red', 'angry', true);
-INSERT INTO bears (name, age, gender, color, temperament. alive) VALUES ('Grinch', 3, 'male', 'red', 'angry', true);
-INSERT INTO bears (name, age, gender, color, temperament. alive) VALUES ('Wendy', 3, 'female', 'red', 'angry', true);
+INSERT INTO bears (name, age, gender, color, temperament. alive) VALUES ('Rowdy', 3, 'male', 'black', 'lazy', true);
+INSERT INTO bears (name, age, gender, color, temperament. alive) VALUES ('Tabitha', 3, 'female', 'grey', 'sleepy', true);
+INSERT INTO bears (name, age, gender, color, temperament. alive) VALUES ('Sergeant Brown', 3, 'male', 'yellow', 'spooked', true);
+INSERT INTO bears (name, age, gender, color, temperament. alive) VALUES ('Melissa', 3, 'female', 'silver', 'confused', true);
+INSERT INTO bears (name, age, gender, color, temperament. alive) VALUES ('Grinch', 3, 'male', 'purple', 'happy', false);
+INSERT INTO bears (name, age, gender, color, temperament. alive) VALUES ('Wendy', 3, 'female', 'blonde', 'sad', true);
