c8a215a83d7b24fbf1ef66d1648cbbd025762a80
Douglas Uretsky <douglasuretsky@gmail.com>
2018-10-30 18:02:27 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -3,6 +3,6 @@ INSERT INTO bears (name, age, gender, color, temperament. alive) VALUES ('Rowdy'
 INSERT INTO bears (name, age, gender, color, temperament. alive) VALUES ('Tabitha', 3, 'male', 'red', 'angry', true);
 INSERT INTO bears (name, age, gender, color, temperament. alive) VALUES ('Sergeant Brown', 3, 'male', 'red', 'angry', true);
 INSERT INTO bears (name, age, gender, color, temperament. alive) VALUES ('Melissa', 3, 'male', 'red', 'angry', true);
-INSERT INTO bears (name, age, gender, color, temperament. alive) VALUES ('Mr. Chocolate', 3, 'male', 'red', 'angry', true);
+INSERT INTO bears (name, age, gender, color, temperament. alive) VALUES ('Grinch', 3, 'male', 'red', 'angry', true);
 INSERT INTO bears (name, age, gender, color, temperament. alive) VALUES ('Mr. Chocolate', 3, 'male', 'red', 'angry', true);
 INSERT INTO bears (name, age, gender, color, temperament. alive) VALUES ('Mr. Chocolate', 3, 'male', 'red', 'angry', true);
